f01d8d873a634621e46aa9f705b354b2432d5afe
rijun <richard.jung001@stud.fh-dortmund.de>
2018-11-21 22:20:36 +0100

    Modified SQL query

--- backend/app.py
+++ backend/app.py
@@ -20,16 +20,15 @@ my_Database = pymysql.connect(
     database="itp_2018_bsp02"  # Michel: itp2
 )
 
+date_format = "%Y-%m-%d"
+month_format = "%Y-%m"
 
-def get_day_values(time, res):
+
+def get_db_values(query):
     date = []
     energy = []
     diff = []
-    next_day = datetime.strptime(time, "%Y-%m-%d") + timedelta(days=1)
     cursor = my_Database.cursor()
-    query = "SELECT DATE_FORMAT(datum_zeit, '%H:%i') AS dzf, obis_180 FROM zaehlwerte " \
-            "WHERE datum_zeit BETWEEN '{0} 00:00:00' AND '{1}' AND MINUTE (datum_zeit) % {2} = 0 " \
-            "ORDER BY datum_zeit ASC".format(time, next_day, res)
     cursor.execute(query)
 
     for result in cursor:
@@ -49,92 +48,57 @@ def get_day_values(time, res):
     return data
 
 
-def get_interval_values(start_time, end_time):
-    date = []
-    energy = []
-    diff = []
-    mycursor = my_Database.cursor()
-    query = (
-        "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'").format(
-        start_time, end_time)
-    mycursor.execute(query)
-    for result in mycursor:
-        date.append(result[0])
-        energy.append(float(result[1]))
-    for i in range(0, len(energy) - 1):
-        diff.append(float(energy[i + 1] - energy[i]))
-    data = {
-        'dates': date,
-        'diff': diff,
-        'energy': energy
-    }
-    return data
-
-
-def get_month_values(year, month):
-    date = []
-    energy = []
-    diff = []
-    mycursor = my_Database.cursor()
-    query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte WHERE YEAR (datum_zeit) = '{0}' AND MONTH (datum_zeit) = '{1}' AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'".format(
-        year, month)
-    mycursor.execute(query)
-    for result in mycursor:
-        date.append(result[0])
-        energy.append(float(result[1]))
-    for i in range(0, len(energy) - 1):
-        diff.append(float(energy[i + 1] - energy[i]))
-    data = {
-        'dates': date,
-        'diff': diff,
-        'energy': energy
-    }
-    return data
-
-
-def get_year_values(time):
-    date = []
-    energy = []
-    diff = []
-    mycursor = my_Database.cursor()
-    query = (
-        "SELECT  DATE_FORMAT (datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte WHERE YEAR (datum_zeit) = '{0}' AND DATE_FORMAT(datum_zeit, '%e') = 1 AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'").format(
-        time)
-    mycursor.execute(query)
-    for result in mycursor:
-        date.append(result[0])
-        energy.append(float(result[1]))
-    for i in range(0, len(energy) - 1):
-        diff.append((floor(energy[i + 1] * 100) - floor(energy[i] * 100)) / 100)
-    data = {
-        'dates': date,
-        'diff': diff,
-        'energy': energy
-    }
-    return data
+def add_month(date):
+    str_date = datetime.strftime(date, month_format)
+    str_list = str_date.split("-")
+    if int(str_list[1]) == 12:
+        next_month = 1
+    else:
+        next_month = str(int(str_list[1]) + 1)
+    return_str = str_list[0] + "-" + next_month
+    return datetime.strptime(return_str, month_format)
 
 
 @app.route('/data')
 def get_data():
     mode = request.args['mode']
+
     if mode == 'day':
-        day = request.args['d']
+        day = datetime.strptime(request.args['d'], date_format)
+        next_day = day + timedelta(days=1)
         res = request.args['r']
-        response = (get_day_values(day, res))
+        query = "SELECT DATE_FORMAT(datum_zeit, '%H:%i'), obis_180 FROM zaehlwerte " \
+                "WHERE datum_zeit BETWEEN '{0} 00:00:00' AND '{1}' AND MINUTE (datum_zeit) % {2} = 0 " \
+                "ORDER BY datum_zeit ASC".format(day.date(), next_day.date(), res)
+        response = get_db_values(query)
+
     elif mode == 'interval':
-        start_time = request.args['st']
-        end_time = request.args['et']
-        response = get_interval_values(start_time, end_time)
+        start_day = datetime.strptime(request.args['sd'], date_format)
+        end_day = datetime.strptime(request.args['ed'], date_format)
+        next_day = end_day + timedelta(days=1)
+        query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
+                "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'".format(start_day.date(), next_day.date())
+        response = get_db_values(query)
+
     elif mode == 'month':
-        month = request.args['m']
-        year = request.args['y']
-        response = get_month_values(year, month)
+        month = datetime.strptime(request.args['m'], month_format)
+        next_month = add_month(month)
+        # year = request.args['y']
+        query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
+                "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'"\
+            .format(month.strftime(date_format), next_month.strftime(date_format))
+        response = get_db_values(query)
+
     elif mode == 'year':
         year = request.args['y']
-        response = get_year_values(year)
+        query = "SELECT  DATE_FORMAT (datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                "WHERE YEAR (datum_zeit) = '{0}' AND DATE_FORMAT(datum_zeit, '%e') = 1 " \
+                "AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'".format(time)
+        response = get_db_values(query)
+
     else:
-        # return make_response(('not_found.html'),404)
-        # def http_404_handler():
         return make_response("<h2>404 Error</h2>", 400)
     return jsonify(response)
 
