8c412affe4cc4e9764755f6c9a7109ec42e297ce
rijun <richard.jung001@stud.fh-dortmund.de>
2018-11-22 21:55:19 +0100

    Added meter number selection

--- backend/app.py
+++ backend/app.py
@@ -24,7 +24,7 @@ my_Database = pymysql.connect(
 date_format = "%Y-%m-%d"
 month_format = "%Y-%m"
 
-meter_dict = {}
+meter_dict = []
 
 def get_db_values(query):
     date = []
@@ -63,61 +63,64 @@ def add_month(date):
 
 @app.route('/data')
 def get_data():
-    mode = request.args['mode']
+    try:
+        mode = request.args['mode']
 
-    if mode == 'day':
-        day = datetime.strptime(request.args['d'], date_format)
-        next_day = day + timedelta(days=1)
-        res = request.args['r']
-        query = "SELECT DATE_FORMAT(datum_zeit, '%H:%i'), obis_180 FROM zaehlwerte " \
-                "WHERE datum_zeit BETWEEN '{0} 00:00:00' AND '{1}' AND MINUTE (datum_zeit) % {2} = 0 " \
-                "ORDER BY datum_zeit ASC".format(day.date(), next_day.date(), res)
-        response = get_db_values(query)
+        if mode == 'day':
+            day = datetime.strptime(request.args['d'], date_format)
+            next_day = day + timedelta(days=1)
+            res = request.args['r']
+            query = "SELECT DATE_FORMAT(datum_zeit, '%H:%i'), obis_180 FROM zaehlwerte " \
+                    "WHERE datum_zeit BETWEEN '{0} 00:00:00' AND '{1}' AND MINUTE (datum_zeit) % {2} = 0 " \
+                    "ORDER BY datum_zeit ASC".format(day.date(), next_day.date(), res)
+            response = get_db_values(query)
 
-    elif mode == 'interval':
-        start_day = datetime.strptime(request.args['sd'], date_format)
-        end_day = datetime.strptime(request.args['ed'], date_format)
-        next_day = end_day + timedelta(days=1)
-        query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
-                "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
-                "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'".format(start_day.date(), next_day.date())
-        response = get_db_values(query)
+        elif mode == 'interval':
+            start_day = datetime.strptime(request.args['sd'], date_format)
+            end_day = datetime.strptime(request.args['ed'], date_format)
+            next_day = end_day + timedelta(days=1)
+            query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                    "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
+                    "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'".format(start_day.date(), next_day.date())
+            response = get_db_values(query)
 
-    elif mode == 'month':
-        month = datetime.strptime(request.args['m'], month_format)
-        next_month = add_month(month)
-        # year = request.args['y']
-        query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
-                "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
-                "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'"\
-            .format(month.strftime(date_format), next_month.strftime(date_format))
-        response = get_db_values(query)
+        elif mode == 'month':
+            month = datetime.strptime(request.args['m'], month_format)
+            next_month = add_month(month)
+            # year = request.args['y']
+            query = "SELECT DATE_FORMAT(datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                    "WHERE DATE_FORMAT(datum_zeit, '%Y-%m-%d') BETWEEN '{0}' AND '{1}' " \
+                    "AND DATE_FORMAT(datum_zeit, '%T') = '00:00:00'"\
+                .format(month.strftime(date_format), next_month.strftime(date_format))
+            response = get_db_values(query)
+        # TODO: not fully implemented yet
+        elif mode == 'year':
+            year = request.args['y']
+            query = "SELECT  DATE_FORMAT (datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
+                    "WHERE YEAR (datum_zeit) = '{0}' AND DATE_FORMAT(datum_zeit, '%e') = 1 " \
+                    "AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'".format(time)
+            response = get_db_values(query)
 
-    elif mode == 'year':
-        year = request.args['y']
-        query = "SELECT  DATE_FORMAT (datum_zeit, '%Y-%m-%d'), obis_180 FROM zaehlwerte " \
-                "WHERE YEAR (datum_zeit) = '{0}' AND DATE_FORMAT(datum_zeit, '%e') = 1 " \
-                "AND DATE_FORMAT(datum_zeit, '%T') = '00:01:00'".format(time)
-        response = get_db_values(query)
-
-    else:
+    except:
         return make_response("<h2>404 Error</h2>", 400)
+
     return jsonify(response)
 
 
 @app.route('/boot')
 def boot():
+    meter_dict.clear()
     cursor = my_Database.cursor()
     query = "SELECT DISTINCT * FROM zaehlpunkte"
     cursor.execute(query)
     for result in cursor:
-        meter_dict[result[0]] = {
+        meter_dict.append({
             'number': result[0],
             'lastname': result[1],
             'firstname': result[2],
             'zipcode': result[3],
             'city': result[4]
-        }
+        })
     return jsonify(meter_dict)
 
 
