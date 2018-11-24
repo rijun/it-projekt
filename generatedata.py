d38e3fea37795532631a6c066e6b044a7babd218
rijun <richard.jung001@stud.fh-dortmund.de>
2018-11-24 23:47:04 +0100

    Min / max filtering added

--- generatedata.py
+++ generatedata.py
@@ -1,8 +1,10 @@
 import csv
 import datetime
+import json
 
 csv_data = []
 
+# Open file
 with open("zaehlwerte.csv", mode='r') as csv_file:
     csv_reader = csv.reader(csv_file)
     line_count = 0
@@ -13,22 +15,42 @@ with open("zaehlwerte.csv", mode='r') as csv_file:
             if row_date.minute % 15 == 0:
                 csv_data.append([row_date, float(row[2])])
         line_count += 1
+    csv_file.close()
 
-    # Convert energy to load
-    for i in range(len(csv_data) - 1):
-        current_data = csv_data[i][1]
-        next_data = csv_data[i + 1][1]
-        diff = round(next_data - current_data, 2)
-        csv_data[i][1] = diff
+# Convert energy to load
+for i in range(len(csv_data) - 1):
+    current_data = csv_data[i][1]
+    next_data = csv_data[i + 1][1]
+    diff = round(next_data - current_data, 2)
+    csv_data[i][1] = diff
 
-    week_dict = {}
+week_dict = {}
 
-    for data in csv_data:
-        current_weekday = data[0].weekday()
-        if current_weekday not in week_dict:
-            week_dict[current_weekday] = {}
-        weekday_dict = week_dict[current_weekday]
-        current_time = data[0].strftime("%H:%M")
-        if current_time not in weekday_dict:
-            weekday_dict[current_time] = []
-        weekday_dict[current_time].append(data[1])
+# Build week dictionary
+for data in csv_data:
+    current_weekday = data[0].weekday()
+    if current_weekday not in week_dict:
+        week_dict[current_weekday] = {}
+    weekday_dict = week_dict[current_weekday]
+    current_time = data[0].strftime("%H:%M")
+    if current_time not in weekday_dict:
+        weekday_dict[current_time] = []
+    weekday_dict[current_time].append(data[1])
+
+data_dict = {}
+
+# Build data dictionary
+for key, value in week_dict.items():
+    current_weekday = key
+    if current_weekday not in data_dict:
+        data_dict[current_weekday] = {}
+    weekday_dict = data_dict[current_weekday]
+    time_list = value.keys()
+    for time in time_list:
+        if time not in weekday_dict:
+            weekday_dict[time] = ()
+        min_load = min(value[time])
+        max_load = max(value[time])
+        weekday_dict[time] = (min_load, max_load)
+
+print(json.dumps(data_dict))
