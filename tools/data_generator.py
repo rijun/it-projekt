385963a5186e2506a01c511bf6e465c417d9a851
rijun <richard.jung001@stud.fh-dortmund.de>
2019-01-13 12:30:01 +0100

    Refactored generate_template() and subfunctions

--- tools/data_generator.py
+++ tools/data_generator.py
@@ -11,8 +11,8 @@ def generate_template():
     Generate a JSON file which maps every weekday and every quarter hour to a minimal, maximal and average load.
     This can later be used as a template for the load profile generation.
 
-    :rtype: dict
     :return: The load profile template
+    :rtype: dict
     """
 
     print("\nReading CSV file... ")
@@ -24,8 +24,8 @@ def generate_template():
 
     print("\nBuilding template... ", end='')
     load_data = convert_energy(csv_data)
-    week_dict = build_week_dict(load_data)
-    profile_dict = build_stat_dict(week_dict)
+    week_dict = get_all_load_diffs(load_data)
+    profile_dict = calculate_load_diffs_statistics(week_dict)
     print("Done!")
 
     print("Writing template file... ", end='')
@@ -38,11 +38,17 @@ def generate_template():
 
 
 def read_csv_data():
+    """
+    Reads a CSV file and returns a list of tuples. The tuples each contain the current datetime
+    and respective meter reading, with an resolution of 15 minutes.
+
+    :return: list of tuples (datetime, meter reading)
+    :rtype: list
+    """
     return_list = []
 
-    # Get csv filename
     filename = input("Name of CSV file (zaehlwerte.csv): ")
-    if not filename:
+    if not filename:    # Default filename
         filename = "zaehlwerte.csv"
 
     try:
@@ -50,7 +56,7 @@ def read_csv_data():
             row_count = sum(1 for row in reader(csv_file))
             print("Number of rows in file: ", row_count)
 
-            csv_file.seek(0)
+            csv_file.seek(0)    # Reset csv_file index to 0
             csv_reader = reader(csv_file)
 
             # Go through each row in the .csv file and append a tuple containing the current date/time
@@ -69,8 +75,17 @@ def read_csv_data():
 
 
 def convert_energy(csv_data):
+    """
+    Convert the meter readings stored in csv_data to load differences (load diffs) by subtracting the current
+    meter reading from the next meter reading.
+
+    :param csv_data: List of tuples (datetime, meter reading)
+    :type csv_data: list
+    :return: List of tuples (datetime, load diffs)
+    :rtype: list
+    """
     load_data = []
-    # Convert energy to load
+
     for i in range(len(csv_data) - 1):  # len(csv_data)-1 as a load can't be calculated for the last element
         current_data = csv_data[i][1]
         next_data = csv_data[i + 1][1]
@@ -79,47 +94,87 @@ def convert_energy(csv_data):
     return load_data
 
 
-def build_week_dict(load_data):
-    week_dict = {}
-    # Build week dictionary
+def get_all_load_diffs(load_data):
+    """
+    Creates a dictionary which contains a list of all load differences sorted by the weekday and time.
+
+    :param load_data: List of tuples (datetime, load diffs)
+    :type load_data: list
+    :return: List of all load diffs sorted by weekday and time
+    :rtype: dict
+    """
+    weekday_dict = {}
+
     for data in load_data:
         current_weekday = data[0].weekday()
-        if current_weekday not in week_dict:
-            week_dict[current_weekday] = {}
-        weekday_dict = week_dict[current_weekday]
+
+        # Add weekday if it is not already in weekday_dict
+        if current_weekday not in weekday_dict:
+            weekday_dict[current_weekday] = {}
+
+        time_dict = weekday_dict[current_weekday]
         current_time = data[0].strftime("%H:%M")
-        if current_time not in weekday_dict:
-            weekday_dict[current_time] = []
-        weekday_dict[current_time].append(data[1])
-    return week_dict
+
+        # Add weekday if it is not already in weekday_dict
+        if current_time not in time_dict:
+            time_dict[current_time] = []
+
+        time_dict[current_time].append(data[1])
+
+    return weekday_dict
 
 
-def build_stat_dict(week_dict):
-    stat_dict = {}
-    # Build data dictionary
+def calculate_load_diffs_statistics(week_dict):
+    """
+    Creates a dictionary which contains a tuple of the minimal, maximal and average load differences of a specific
+    weekday and time. The return dictionary statistics_dict is sorted by weekday and time.
+
+    :param week_dict: All load diffs sorted by weekday and time
+    :type week_dict: dict
+    :return: Min, max and average load diffs sorted by weekday and time
+    :rtype: dict
+    """
+    statistics_dict = {}
+
     for key, value in week_dict.items():
         current_weekday = key
-        if current_weekday not in stat_dict:
-            stat_dict[current_weekday] = {}
-        weekday_dict = stat_dict[current_weekday]
-        time_list = value.keys()
-        for time in time_list:
+
+        # Add weekday if it is not already in statistics_dict
+        if current_weekday not in statistics_dict:
+            statistics_dict[current_weekday] = {}
+
+        weekday_dict = statistics_dict[current_weekday]
+        load_diffs_list = value.keys()
+
+        for time in load_diffs_list:
+            # Add time if it is not already in load_diffs_list
             if time not in weekday_dict:
                 weekday_dict[time] = ()
-            min_load = min(value[time])
-            max_load = max(value[time])
-            avg_load = mean(value[time])
-            weekday_dict[time] = (min_load, max_load, avg_load)
-    return stat_dict
+
+            min_load_diff = min(value[time])
+            max_load_diff = max(value[time])
+            avg_load_diff = mean(value[time])
+
+            weekday_dict[time] = (min_load_diff, max_load_diff, avg_load_diff)
+
+    return statistics_dict
 
 
 def generate_load_profile(template=None):
+    """
+    Generate a JSON file which maps every weekday and every quarter hour to a minimal, maximal and average load.
+    This can later be used as a template for the load profile generation.
+
+    :rtype: dict
+    :return: The load profile template
+    """
 
     # Load JSON template if it is not passed as an argument
     if not template:
         filename = input("Name of JSON file (template.json): ")
-        if not filename:
+        if not filename:    # Default filename
             filename = "template.json"
+
         print("Opening template file... ", end='')
         with open(filename, mode='r') as json_file:
             template = load(json_file)
@@ -128,15 +183,24 @@ def generate_load_profile(template=None):
     csv_entry_list = build_load_profile(template)
 
     print("Writing load profile... ", end='')
-    with open("data.csv", mode='w') as csv_file:
+    with open("data.csv", mode='w', newline="\n", encoding="utf-8") as csv_file:
         csv_writer = writer(csv_file, delimiter=',', quotechar='"', quoting=QUOTE_NONE)
         for row in csv_entry_list:
             csv_writer.writerow(row)
         csv_file.close()
+
     print("Done!")
+    input("Press <Enter> to exit...")
 
 
 def build_load_profile(template):
+    """
+    Generate a JSON file which maps every weekday and every quarter hour to a minimal, maximal and average load.
+    This can later be used as a template for the load profile generation.
+
+    :rtype: dict
+    :return: The load profile template
+    """
 
     start, end, meter_number, meter_start_val = get_user_settings()
 
@@ -146,14 +210,15 @@ def build_load_profile(template):
     START_DATE = datetime.strptime(start, "%Y-%m-%d")
     END_DATE = datetime.strptime(end, "%Y-%m-%d")
     current_datetime = START_DATE
-    current_meter_val = meter_start_val
+    current_meter_val = 0
 
     while current_datetime <= END_DATE:
         seed()
         month_factor = 0.25 * cos(2 * pi / 12 * (current_datetime.month - 0.5)) + 1.50
 
         if current_datetime == START_DATE:
-            load_profile_entry = [current_datetime, meter_number, meter_start_val * month_factor, 0]
+            current_meter_val = meter_start_val * month_factor
+            load_profile_entry = [current_datetime, meter_number, round(current_meter_val, 2), 0]
         else:
             current_weekday = current_datetime.weekday()
             current_time = current_datetime.time().strftime("%H:%M")
@@ -173,7 +238,13 @@ def build_load_profile(template):
 
 
 def get_user_settings():
-    # Get user input
+    """
+    Generate a JSON file which maps every weekday and every quarter hour to a minimal, maximal and average load.
+    This can later be used as a template for the load profile generation.
+
+    :rtype: dict
+    :return: The load profile template
+    """
     start = input("Start date in YYYY-MM-DD format (2018-01-01): ")
     if not start:
         start = "2018-01-01"
@@ -191,6 +262,14 @@ def get_user_settings():
 
 
 def menu():
+    """
+    Generate a JSON file which maps every weekday and every quarter hour to a minimal, maximal and average load.
+    This can later be used as a template for the load profile generation.
+
+    :rtype: dict
+    :return: The load profile template
+    """
+
     print("## Meter Readings Generation Tool ##\n")
     print("Select an option:\n")
     print("(1)\t\t-->\t\tGenerate template and database file (default)")
@@ -210,7 +289,7 @@ def menu():
         except ValueError:
             print("Input error!")
             input("Press <Enter> to exit...")
-            quit()
+            quit(-1)
 
 
 if __name__ == "__main__":
