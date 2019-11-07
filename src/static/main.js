4e7ff8052136b441df35ae2a95d0218cab329dea
rijun <richard.jung001@stud.fh-dortmund.de>
2019-11-07 10:43:26 +0100

    Added fake chart

--- src/static/main.js
+++ src/static/main.js
@@ -1,5 +1,47 @@
 // Setup event handlers
-document.getElementById("sendButton").onclick = () => { console.log("Button") };
+document.getElementById('sendButton').onclick = () => {
+    document.getElementById('meterSelection').classList.add('d-none');
+    document.getElementById('meterDisplay').classList.remove('d-none');
+    const ctx = document.getElementById('myChart').getContext('2d');
+    let myChart = new Chart(ctx, {
+        type: 'bar',
+        data: {
+            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            datasets: [{
+                label: '# of Votes',
+                data: [12, 19, 3, 5, 2, 3],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    $('#queryModal').modal('hide');
+};
+
 document.getElementById('meterSelector').onchange = setSelectorRanges;
 
 function setupMeterSelector() {
