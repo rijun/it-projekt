50a6ea96d6cf3aeffe919e25341cd178c46c88f6
rijun <richard.jung001@stud.fh-dortmund.de>
2019-11-27 22:21:54 +0100

    First chart implementation

--- src/static/main.js
+++ src/static/main.js
@@ -10,7 +10,6 @@ document.getElementById('sendButton').onclick = () => {
 };
 
 document.getElementById('meterSelector').onchange = setSelectorRanges;
-document.getElementById('meterReadingsSelector').onchange = meterReadingsSelectorChanged;
 
 window.onresize = () => {
     if (window.chart === undefined) {
@@ -57,90 +56,8 @@ function setSelectorRanges() {
         "<option value=\"" + moment(meterDates.min).format("YYYY") + "\">" + moment(meterDates.min).format("YYYY") + "</option>";
 }
 
-function setupChart() {
-    /**
-     * Setup chart with the settings stored in the chart_settings.js file
-     * **/
-
-    const ctx = document.getElementById("chart").getContext('2d');
-    window.chart = new Chart(ctx, {
-        type: "bar",
-        data: {
-            datasets: [{
-                label: "Lastgang",
-                backgroundColor: "rgba(255, 128, 0, 0.2)",
-                borderColor: "rgb(255, 128, 0)",
-                borderWidth: 1,
-                yAxisID: "y-axis-load",
-            }]
-        },
-        options: {
-            scales: {
-                xAxes: [{
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Zeitpunkt",
-                    },
-                    gridLines: {
-                        offsetGridLines: true
-                    }
-                }],
-                yAxes: [{
-                    type: "linear",
-                    display: true,
-                    position: "left",
-                    id: "y-axis-load",
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Lastgang",
-                    },
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }, {
-                    type: "linear",
-                    display: false,
-                    position: "right",
-                    id: "y-axis-energy",
-                    scaleLabel: {
-                        display: true,
-                        labelString: "Zählerstand [kWh]",
-                    },
-                    gridLines: {
-                        drawOnChartArea: false,
-                    }
-                }]
-            },
-            tooltips: {
-                mode: "index",
-                // Custom tooltip settings
-                callbacks: {
-                    label: function (tooltipItem, data) {
-                        if (tooltipItem.datasetIndex === 0) {
-                            return tooltipItem.yLabel + ' ' + getCurrentUnit();
-                        } else {
-                            return tooltipItem.yLabel + " kWh";
-                        }
-                    },
-                    footer: function (tooltipItem, data) {
-                        let kwhPrice;
-                        let load = tooltipItem[0].yLabel;
-                        // let price = document.getElementById("priceSelector").value / 100;
-                        let price = 0.3;
-                        kwhPrice = calculatePrice(load, price);
-                        return "Kosten: " + kwhPrice + ' €';
-                    },
-                },
-                footerFontStyle: "normal"
-            }
-        }
-    });
-}
-
 // Run on startup
 window.onload = function () {
-    moment.locale('de');    // Set Moment.js to german language
-    addMetersToStorage();
     setupMeterSelector();
     setMeterButtonEvents();
     setupChart();
@@ -198,7 +115,6 @@ function updatePage() {
     // Update page contents
     updateHeader();
     updateChart();
-    updateStatistics(price);
 }
 
 function updateHeader() {
@@ -307,147 +223,6 @@ function buildUserInfoHeader() {
     //     }
     // }
     document.getElementById("userInfo").innerHTML = "Max Mustermann - Dortmund (44139)";
-}
-
-/* Chart Update */
-function updateChart() {
-    /**
-     * Update the chart according to the current response data
-     * **/
-
-    assignChartXValues();
-    assignChartYValues();
-    window.chart.update();
-}
-
-function assignChartXValues() {
-    let formattedLabels = [];
-
-    switch (window.state) {
-        case States.day: // state == day
-            window.currentMeter.datetimes.forEach(t => formattedLabels.push(t.format("LT")));
-            break;
-        case States.interval: // state == interval
-        case States.month: // state == month
-            window.currentMeter.datetimes.forEach(t => formattedLabels.push(t.format("L")));
-            break;
-        case States.year: // state == year
-            window.currentMeter.datetimes.forEach(t => formattedLabels.push(t.format("MMMM")));
-    }
-
-    window.chart.data.labels = formattedLabels;
-}
-
-function assignChartYValues() {
-    window.chart.data.datasets[0].data = window.currentMeter.loadDiffs;  // Add loadDiffs to chart
-
-    if (document.getElementById('meterReadingsSelector').checked) {
-        window.chart.data.datasets[1].data = window.currentMeter.meterReadings;  // Add meterReadings to chart
-    }
-
-    window.chart.options.scales.yAxes[0].scaleLabel.labelString = "Lastgang [" + getCurrentUnit() + "]";
-}
-
-/* Statistics Update */
-function updateStatistics(kwhPrice) {
-    /**
-     * Update the statistics data according to the current response data
-     * **/
-
-    document.getElementById("statData").innerHTML =
-        "<div class=\"col-md m-2 p-2 border border-secondary rounded\"><h6>Durchschnittsverbrauch</h6>" + window.currentMeter.avgKwh + " " + getCurrentUnit() + "</div>"
-        + "<div class=\"col-md m-2 p-2 border border-secondary rounded\"><h6>Maximalverbrauch</h6>" + window.currentMeter.maxKwh + " " + getCurrentUnit() + "</div>"
-        + "<div class=\"col-md m-2 p-2 border border-secondary rounded\"><h6>Minimalverbrauch</h6>" + window.currentMeter.minKwh + " " + getCurrentUnit() + "</div>"
-        + "<div class=\"col-md m-2 p-2 border border-secondary rounded\"><h6>Gesamtverbrauch</h6>" + window.currentMeter.sumKwh + " kWh</div>"
-        + "<div class=\"col-md m-2 p-2 border border-secondary rounded\"><h6>Gesamtkosten</h6>" + "<span id=\"statPrice\">"
-        + roundTwoPlaces(window.currentMeter.sumKwh * kwhPrice) + "</span> €</div>";
-}
-
-function modeChanged() {
-    /**
-     * Change UI and state according to the user selection
-     * **/
-
-    const modeSelector = document.getElementById('mode-selector');
-
-    if (modeSelector.value === "day") {
-        setStateToDay();
-    } else if (modeSelector.value === "interval") {
-        setStateToInterval();
-    } else if (modeSelector.value === "month") {
-        setStateToMonth();
-    } else {
-        setStateToYear();
-    }
-}
-
-function setStateToDay() {
-    document.getElementById("day-options").style.display = "block";
-    document.getElementById("interval-options").style.display = "none";
-    document.getElementById("month-options").style.display = "none";
-    document.getElementById("year-options").style.display = "none";
-    state = 1; // Set state to day
-}
-
-function setStateToInterval() {
-    document.getElementById("day-options").style.display = "none";
-    document.getElementById("interval-options").style.display = "block";
-    document.getElementById("month-options").style.display = "none";
-    document.getElementById("year-options").style.display = "none";
-    state = 2; // Set state to custom
-}
-
-function setStateToMonth() {
-    document.getElementById("day-options").style.display = "none";
-    document.getElementById("interval-options").style.display = "none";
-    document.getElementById("month-options").style.display = "block";
-    document.getElementById("year-options").style.display = "none";
-    state = 3; // Set state to month
-}
-
-function setStateToYear() {
-    document.getElementById("day-options").style.display = "none";
-    document.getElementById("interval-options").style.display = "none";
-    document.getElementById("month-options").style.display = "none";
-    document.getElementById("year-options").style.display = "block";
-    state = 4; // Set state to year
-}
-
-function priceChanged() {
-    /**
-     * Change kWh price according to the user selection
-     * **/
-
-    let currentPrice = document.getElementById("priceInput").value / 100;
-    document.getElementById("statPrice").innerText = roundTwoPlaces(window.currentMeter.sumKwh * currentPrice);
-}
-
-function meterReadingsSelectorChanged() {
-    /**
-     * Add and remove the meter value line graph from the chart
-     * **/
-
-    if (this.checked) { // Add line graph
-        if (window.chart.data.datasets.length === 1) {   // Add line graph if not already in chart
-            window.chart.data.datasets.push({
-                label: 'Zählerstand',
-                data: window.currentMeter.meterReadings,
-                backgroundColor: "rgba(255, 0, 0, 0.2)",
-                borderColor: "rgb(255, 0, 0)",
-                borderWidth: 1,
-                type: "line",
-                yAxisID: 'y-axis-energy'
-            });
-            window.chart.options.scales.yAxes[1].display = true;
-        } else {
-            window.chart.data.datasets[1].data = window.currentMeter.meterReadings;  // Update line graph values if already in chart
-        }
-    } else if (window.chart.data.datasets.length === 2) {    // Remove line graph if already in chart
-        window.chart.data.datasets.pop();
-        window.chart.options.scales.yAxes[1].display = false;
-    }
-
-    window.chart.update();
 }
 
 function checkInputs() {
