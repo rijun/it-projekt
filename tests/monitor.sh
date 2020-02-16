0d5b991e047091711224a7cfed5ba6bd35b8f22e
rijun <richard.jung001@stud.fh-dortmund.de>
2020-02-16 22:46:38 +0100

    Fixed counting bug

--- tests/monitor.sh
+++ tests/monitor.sh
@@ -3,7 +3,7 @@
 cd "${0%/*}" || exit
 
 response=$(curl --write-out "%{http_code}\n" --silent --head --output /dev/null http://localhost:5000)
-run_no=$((${$(sed -n '1p' log.txt): -1}+1))
+run_no=$(($(sed -n '1p' log.txt | grep -Eo '[0-9]{1,}')+1))
 logs=$(sed '1,2d' log.txt)
 
 echo "No. of runs: ${run_no}" > log.txt
